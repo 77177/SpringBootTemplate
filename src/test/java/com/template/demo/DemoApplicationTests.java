34a71da20da1b63c2b706f13c8df1d0f22897989
stanislav <iluvstrwrs@mail.ru>
2020-01-05 00:47:47 +0300

    added test

--- src/test/java/com/template/demo/DemoApplicationTests.java
+++ src/test/java/com/template/demo/DemoApplicationTests.java
@@ -4,10 +4,12 @@ import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.authentication.FormAuthConfig;
 import com.jayway.restassured.authentication.FormAuthScheme;
 import com.jayway.restassured.http.ContentType;
+import com.jayway.restassured.response.ExtractableResponse;
 import com.jayway.restassured.response.Response;
 import com.template.demo.person.models.Person;
 import com.template.demo.person.services.PersonService;
 import org.hamcrest.Matchers;
+import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -17,7 +19,7 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static org.junit.Assert.assertEquals;
+import java.util.List;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -47,18 +49,64 @@ class DemoApplicationTests {
     }
 
     @Test
-    public void testPost() {
+    public void testPostGetPutDelete() {
+
+        Person person = new Person(1,"First","Last");
+
+        //POST TEST ----------------------------------------------------------------------------------------------
+        ExtractableResponse<Response> postResponse = RestAssured.given().contentType(ContentType.JSON)
+                .body(person)
+                .post("/persons/create")
+                .then()
+                .statusCode(200)
+                .extract();
+        //---------------------------------------------------------------------------------------------------------
+
+        long id = Long.parseLong(postResponse.response().body().asString());
+
+        //GET TEST ----------------------------------------------------------------------------------------------
+        RestAssured.given().contentType(ContentType.JSON)
+                .get("/persons/get/" + id)
+                .then()
+                .body("firstName", Matchers.is(person.getFirstName()))
+                .body("lastName", Matchers.is(person.getLastName()))
+                .statusCode(200);
+        //---------------------------------------------------------------------------------------------------------
+
+        Person personUpdated = new Person(id,"NewFirst","NewLast");
+
+        //PUT TEST ----------------------------------------------------------------------------------------------
+        ExtractableResponse<Response> putResponse = RestAssured.given().contentType(ContentType.JSON)
+                .body(personUpdated)
+                .put("/persons/update")
+                .then()
+                .statusCode(200)
+                .extract();
+        //---------------------------------------------------------------------------------------------------------
+
+        long updateId = Long.parseLong(putResponse.response().body().asString());
+
+        Assert.assertEquals(id,updateId);
+
+        RestAssured.given().contentType(ContentType.JSON)
+                .get("/persons/get/" + updateId)
+                .then()
+                .body("firstName", Matchers.is(personUpdated.getFirstName()))
+                .body("lastName", Matchers.is(personUpdated.getLastName()))
+                .statusCode(200);
+
+        int sizeBeforeDelete = personService.getAllPersons().size();
+
+        //DELETE TEST ----------------------------------------------------------------------------------------------
+        RestAssured.given().contentType(ContentType.JSON)
+                .delete("/persons/delete/" + updateId)
+                .then()
+                .statusCode(200);
+        //---------------------------------------------------------------------------------------------------------
+
+        int sizeAfterDelete = personService.getAllPersons().size();
+
+        Assert.assertEquals((1), sizeBeforeDelete - sizeAfterDelete);
     }
 
-    @Test
-    public void testGet() {
-    }
-
-    @Test
-    public void testPut() {
-    }
-
-    @Test
-    public void testDelete() {
-    }
 }
