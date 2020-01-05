d17c9cc0dff182dadab48e44e7f52b4ea454d5eb
stanislav <iluvstrwrs@mail.ru>
2020-01-05 22:41:36 +0300

    added text generation

--- test_data_generator/src/main/java/com/test/TestDataGenerator.java
+++ test_data_generator/src/main/java/com/test/TestDataGenerator.java
@@ -5,15 +5,25 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class TestDataGenerator {
 
     public Field setField(Object o, Field f) throws IllegalAccessException {
 
         f.setAccessible(true);
-
+        Random randomGenerator = new Random();
         if (f.getType().equals(String.class)) {
-            f.set(o, "test");
+            String string = Stream.iterate(0, i -> ++i)
+                    .limit(20)
+                    .map(integer -> randomGenerator.nextInt() % 100)
+                    .mapToInt(integer -> integer)
+                    .mapToObj(i -> (char) (i))
+                    .map(String::valueOf)
+                    .collect(Collectors.joining());
+            f.set(o, string);
         } else if ((f.getType().equals(Integer.class))){
             f.set(o, (int) (Math.random() * 1000));
         } else if ((f.getType().equals(Long.class))){
